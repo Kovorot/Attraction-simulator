59fbf7c5bcc526ee4b0452ecc0ac195f06035f99
Andrey <kovorot0875@mail.ru>
2023-03-21 16:29:10 +0300

    Basic List method added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
+
+    public static List list = new LinkedList();
     public static void main(String[] args) {
+
+    }
+
+    public static List<Person> generateClients() {
     }
 }
